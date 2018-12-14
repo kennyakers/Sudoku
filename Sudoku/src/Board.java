3e2ea1ef1369063908f1bfbd32d6ad1b175b5782
kennyakers <akers.kenneth66@gmail.com>
2018-12-14 15:45:50 -0800

    Sync.

--- Sudoku/src/Board.java
+++ Sudoku/src/Board.java
@@ -1,68 +1,58 @@
 
-import java.util.Arrays;
-
 public class Board {
 
     private int[][] board;
+
+    private boolean debug = true;
 
     public Board(int[][] inputBoard) {
         this.board = inputBoard;
     }
 
     public boolean isGoalState() {
-        int sum = 0; 
-        for(int x = 0; x < board.length; x++){
-            for(int y = 0; y < board.length; y++){
-                
+        int sumRow = 0;
+        int sumColumn = 0;
+        int desiredSum = 0;
+        for (int i = this.board.length; i > 0; i--) {
+            desiredSum += i;
+        }
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board.length; y++) {
+                if (this.board[x][y] == 0 || this.board[y][x] == 0) {
+                    return false;
+                }
+                sumRow += this.board[x][y];
+                sumColumn += this.board[y][x];
             }
+            if (!(sumRow == desiredSum && sumColumn == desiredSum)) {
+                return false;
+            }
+            sumRow = 0;
+            sumColumn = 0;
         }
         return true;
     }
-    
+
     public boolean isLegal(int x, int y, int number) {
-
-        boolean[] rowValues = new boolean[9];
-        boolean[] columnValues = new boolean[9];
-
-        for (int i = 0; i < board.length; i++) {
-            columnValues[this.board[x][i]] = true;
-            rowValues[this.board[i][y]] = true;
-        }
-        for (int i = 0; i < rowValues.length; i++) {
-            if (!(columnValues[i] && rowValues[i])) {
+        for (int i = 0; i < this.board[0].length; i++) {
+            if (this.board[x][i] == number || this.board[i][y] == number) {
                 return false;
             }
         }
-        return true;
+        return this.board[x][y] == 0;
     }
 
     public boolean move(int x, int y, int number) {
-        for(int i = 0; i < board.length; i++){
-            if(board[x][i] == number || board[i][y] == number)
-                return false;
-        }
-        if(board[x][y] == 0){
-            board[x][y] = number;
+        if (this.isLegal(x, y, number)) {
+            this.board[x][y] = number;
             return true;
         }
         return false;
     }
 
-    
-
-    private Board copy() {
-//        int[][] array = new int[board.length][board.length];
-//        for (int i = 0; i < this.board.length; i++ ) {
-//            for (int j = 0; j < this.board[0].length; j++) {
-//                array[i][j] = this.board[i][j];
-//            }
-//        }
-//        return new Board(array);
-        return new Board(this.board);
-    }
-    
     public void print() {
-        for (int i = 0; i < this.board.length; i++ ) {
+        System.out.println("");
+        for (int i = 0; i < this.board.length; i++) {
             for (int j = 0; j < this.board[0].length; j++) {
                 System.out.print(this.board[i][j] + " ");
             }
@@ -70,34 +60,29 @@ public class Board {
         }
     }
 
-    public Board solve(int x, int y, int value, Board inputBoard) {
-        Board copy = inputBoard.copy();
-        if (copy.isGoalState()) {
-            return copy;
+    public boolean solve(int x, int y, int value) {
+        if (this.isGoalState()) {
+            return true;
         }
-        boolean result = copy.move(x, y, value);
-        if(board[x][y] == value){
-            result = true;
+
+        
+
+        //if goalState return true
+        //do the modular arithmetic for next move (rows = / , columns = %)
+        //attempt the requested move
+        //if it works
+            //for all possible values attempt to solve for next move
+            //at the end of the for loop if we never got a true back
+                //set that coordinate to zero
+                //return false
+            //if we got a true back
+                //return true!
+        //if fails
+            //return false
+        for (int i = 1; i <= board.length; i++) {
+            boolean result = solve(x, y, i);
         }
         
-        System.out.println("Move: " + x + " " + y + " " + value + " " + result);
-        copy.print();
-        if (!result) {
-            return copy;
-        }
-        for (int i = 1; i <= copy.board.length; i++) {
-            //0,0
-            int xNext = x + 1;
-            int yNext = y;
-            if (xNext == board.length) {
-                xNext = 0;
-                yNext = y + 1;
-            }
-            Board attempt = this.solve(xNext, yNext, i, copy);
-            if (!Arrays.equals(attempt.board, copy.board)) {
-                return attempt;
-            }
-        }
-        return copy;
     }
+
 }
--- Sudoku/src/Tester.java
+++ Sudoku/src/Tester.java
@@ -1,3 +1,4 @@
+
 /**
  * Kenny Akers
  * Mr. Paige
@@ -8,12 +9,25 @@ public class Tester {
 
     public static void main(String[] args) {
         int[][] array = {
-            {1, 2, 3},
-            {2, 3, 1},
-            {3, 1, 2}
+            {0, 0, 0, 3},
+            {0, 0, 3, 0},
+            {0, 3, 0, 0},
+            {3, 0, 0, 0}
         };
         Board board = new Board(array);
-        board.solve(0, 0, 1, board).print();
+        if (board.solve(0, 0, 1)) {
+            board.print();
+        }
+        else {
+            System.out.println("Did not solve");
+        }
     }
 
+    /*
+    4 5 1 2 3
+    5 1 2 3 4
+    1 2 3 4 5
+    2 3 4 5 1
+    3 4 5 1 2
+     */
 }
