bf2c37adb8c9eb260eac19449682707034ab6a90
kennyakers <akers.kenneth66@gmail.com>
2018-12-15 13:37:04 -0800

    Done.

--- Sudoku/src/Board.java
+++ Sudoku/src/Board.java
@@ -3,86 +3,91 @@ public class Board {
 
     private int[][] board;
 
-    private boolean debug = true;
-
     public Board(int[][] inputBoard) {
         this.board = inputBoard;
     }
 
-    public boolean isGoalState() {
-        int sumRow = 0;
-        int sumColumn = 0;
-        int desiredSum = 0;
-        for (int i = this.board.length; i > 0; i--) {
-            desiredSum += i;
+    public boolean isLegal(int row, int column, int value) {
+        // Check rows and columns
+        for (int i = 0; i < this.board[0].length; i++) {
+            if (this.board[row][i] == value || this.board[i][column] == value) {
+                return false;
+            }
         }
-        for (int x = 0; x < board.length; x++) {
-            for (int y = 0; y < board.length; y++) {
-                if (this.board[x][y] == 0 || this.board[y][x] == 0) {
+
+        // Check squares
+        int boxDimension = (int) Math.sqrt(this.board.length);
+        int boxRow = row - (row % boxDimension); // Starting row index for the box containing this (row, column).
+        int boxColumn = column - (column % boxDimension); // Starting column index for the box containing this (row, column).
+        for (int i = boxRow; i < boxRow + boxDimension; i++) {
+            for (int j = boxColumn; j < boxColumn + boxDimension; j++) {
+                if (this.board[i][j] == value) {
                     return false;
                 }
-                sumRow += this.board[x][y];
-                sumColumn += this.board[y][x];
-            }
-            if (!(sumRow == desiredSum && sumColumn == desiredSum)) {
-                return false;
-            }
-            sumRow = 0;
-            sumColumn = 0;
-        }
-        return true;
-    }
-
-    public boolean isLegal(int x, int y, int number) {
-        for (int i = 0; i < this.board[0].length; i++) {
-            if (this.board[x][i] == number || this.board[i][y] == number) {
-                return false;
             }
         }
-        return this.board[x][y] == 0;
+        // Make sure it's blank
+        return this.board[row][column] == 0;
     }
 
-    public boolean move(int x, int y, int number) {
-        if (this.isLegal(x, y, number)) {
-            this.board[x][y] = number;
+    public boolean solve() {
+        int blankRow = -1;
+        int blankColumn = -1;
+        boolean solved = true;
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[0].length; j++) {
+                if (this.board[i][j] == 0) {
+                    blankRow = i;
+                    blankColumn = j;
+                    solved = false;
+                    break;
+                }
+            }
+            if (!solved) {
+                break;
+            }
+        }
+
+        if (solved) {
+            // We're done, so return true
             return true;
+        }
+
+        // else for each-row backtrack 
+        for (int value = 1; value <= this.board.length; value++) {
+            if (this.isLegal(blankRow, blankColumn, value)) {
+                board[blankRow][blankColumn] = value;
+                if (this.solve()) {
+                    return true;
+                } else {
+                    board[blankRow][blankColumn] = 0; // Value didn't work, so erase it.
+                }
+            }
         }
         return false;
     }
 
     public void print() {
-        System.out.println("");
-        for (int i = 0; i < this.board.length; i++) {
-            for (int j = 0; j < this.board[0].length; j++) {
-                System.out.print(this.board[i][j] + " ");
+        int rowLength = 0;
+        for (int row = 0; row < this.board.length; row++) {
+            for (int column = 0; column < this.board.length; column++) {
+                if (column != 0 && column % (int) Math.sqrt(this.board.length) == 0) {
+                    System.out.print("| ");
+                    rowLength += 2;
+                }
+                System.out.print(this.board[row][column] + " ");
+                rowLength += 2;
             }
             System.out.println("");
-        }
-    }
 
-    public boolean solve(int x, int y, int value) {
-        if (this.isGoalState()) {
-            return true;
+            if ((row + 1) % (int) Math.sqrt(board.length) == 0 && row != this.board.length - 1) {
+                for (int i = 0; i < rowLength; i++) {
+                    System.out.print("-");
+                }
+                System.out.println("");
+            }
+            rowLength = 0;
         }
-
-        
-
-        //if goalState return true
-        //do the modular arithmetic for next move (rows = / , columns = %)
-        //attempt the requested move
-        //if it works
-            //for all possible values attempt to solve for next move
-            //at the end of the for loop if we never got a true back
-                //set that coordinate to zero
-                //return false
-            //if we got a true back
-                //return true!
-        //if fails
-            //return false
-        for (int i = 1; i <= board.length; i++) {
-            boolean result = solve(x, y, i);
-        }
-        
     }
 
 }
--- Sudoku/src/Tester.java
+++ Sudoku/src/Tester.java
@@ -8,19 +8,27 @@
 public class Tester {
 
     public static void main(String[] args) {
+
         int[][] array = {
-            {0, 0, 0, 3},
-            {0, 0, 3, 0},
-            {0, 3, 0, 0},
-            {3, 0, 0, 0}
+            {3, 0, 6, 5, 0, 8, 4, 0, 0},
+            {5, 2, 0, 0, 0, 0, 0, 0, 0},
+            {0, 8, 7, 0, 0, 0, 0, 3, 1},
+            {0, 0, 3, 0, 1, 0, 0, 8, 0},
+            {9, 0, 0, 8, 6, 3, 0, 0, 5},
+            {0, 5, 0, 0, 9, 0, 6, 0, 0},
+            {1, 3, 0, 0, 0, 0, 2, 5, 0},
+            {0, 0, 0, 0, 0, 0, 0, 7, 4},
+            {0, 0, 5, 2, 0, 6, 3, 0, 0}
         };
+
         Board board = new Board(array);
-        if (board.solve(0, 0, 1)) {
+
+        if (board.solve()) {
             board.print();
+        } else {
+            System.out.println("No solution");
         }
-        else {
-            System.out.println("Did not solve");
-        }
+
     }
 
     /*
